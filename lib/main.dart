78740bd9c2fc48454d0e516eff89fdb542b9fb1e
Marvin Brückner <marvin.brueckner@gmx.net>
2020-09-15 19:44:53 +0200

    Some methods

--- lib/main.dart
+++ lib/main.dart
@@ -23,15 +23,27 @@ class TextInputWidget extends StatefulWidget {
 
 class _TextInputWidgetState extends State<TextInputWidget> {
   final controller = TextEditingController();
+  String text = "";
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void changeText(text) {
+    this.text = text;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
       TextField(
       controller: this.controller,
-      decoration: InputDecoration(prefixIcon: Icon(Icons.message),
+        decoration: InputDecoration(prefixIcon: Icon(Icons.message),
       labelText: "Sende eine Nachricht"),
-      ), Text(controller.text)
+        onChanged: (text) => this.changeText(text),
+      ), Text(this.text)
     ]);
   }
 }
